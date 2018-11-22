b9e1cdf5a137aef7bddfa3d7827b4a541bf5cce9
Jasmine English <thisjasmine0@gmail.com>
2018-11-22 15:37:03 -0500

    added models and schemas for blog

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,37 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_LOCATION);
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DB_LOCATION)
 
-const Article = mongoose.model('Article', {
-  title: String
-});
+const tagSchema = new mongoose.Schema({
+  name: String
+})
 
-const db = mongoose.connection;
+const imageSchema = new mongoose.Schema({
+  url: String,
+  description: String
+})
 
-export default db;
+const articleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  entry: String,
+  headerImage: imageSchema,
+  images: [imageSchema],
+  tags: [tagSchema],
+  datePublished: Date,
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+  dateUpdated: Date,
+  dateDeleted: Date
+})
+
+const Article = mongoose.model('Article', articleSchema)
+const Tag = mongoose.model('Tag', tagSchema)
+const Image = mongoose.model('Image', imageSchema)
+
+const db = mongoose.connection
+module.exports = {db}
